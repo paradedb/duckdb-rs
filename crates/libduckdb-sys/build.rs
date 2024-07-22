19a93fb739179df97e4d062c41deb5ab42ada09c
Ming Ying <ming.ying.nyc@gmail.com>
2024-07-22 14:09:49 -0400

    Add icu

--- crates/libduckdb-sys/build.rs
+++ crates/libduckdb-sys/build.rs
@@ -117,6 +117,9 @@ mod build_bundled {
 
         let mut cfg = cc::Build::new();
 
+        #[cfg(feature = "icu")]
+        add_extension(&mut cfg, &manifest, "icu", &mut cpp_files, &mut include_dirs);
+
         #[cfg(feature = "parquet")]
         add_extension(&mut cfg, &manifest, "parquet", &mut cpp_files, &mut include_dirs);
 
